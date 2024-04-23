ee8bcc310184adc58911b3dee481e7e25c73012b
Shubhank Sharma <shubhanksharma345@gmail.com>
2024-04-23 20:12:36 +0530

    CreateWorkoutsController created

--- backend/controllers/WorkoutsControllers.js
+++ backend/controllers/WorkoutsControllers.js
@@ -1,5 +1,20 @@
 import WorkoutsModel from "../models/WorkoutsModel.js"
 
 export const createWorkoutsController = async(req,resp)=>{
-    
+    try {
+       const {title,load,reps} = req.body
+     const workout =await WorkoutsModel.create({title,load,reps})
+     resp.json({
+        success:true,
+        message:"Workout created",
+        workout
+     }).status(200)
+   } catch (error) {
+    resp.json({
+        success:false,
+        message:"Error in creating a Workout",
+        error
+    }).status(400)
+    console.log(error)
+   }
 }
