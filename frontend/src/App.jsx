99825526a8185d80707bd2102efc5521351ced3a
Shubhank Sharma <shubhanksharma345@gmail.com>
2024-04-24 21:14:13 +0530

    Navbar component created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <>
+    <div className="App">
       <BrowserRouter>
+        <Navbar />
         <Routes>
-          <Route path="/" element={<h1>This is home page </h1>} />
+          <Route path="" element={<h1>This is home page </h1>} />
         </Routes>
       </BrowserRouter>
-    </>
+    </div>
   );
 }
 
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <header>
+      <div className="container">
+        <NavLink to="/">Workout</NavLink>
+      </div>
+    </header>
+  );
+}
+
+export default Navbar;
